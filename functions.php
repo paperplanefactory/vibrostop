fa711e678316ceec970f2722586bb51f40f8e8ef
paperplanefactory <giovanni@paperplanefactory.com>
2021-02-08 18:20:31 +0100

    Theme version updated

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 // imposto la versione del tema
 global $theme_version;
-$theme_version = 4.1;
+$theme_version = 4.2;
 // gestione caricamento css
 include_once "functions/theme_stylesloader.php";
 // gestione caricamento script
